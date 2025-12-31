437511c6dabe19ed14ee9ed7267133481837b81f
ashisdutta <ashisdutta795@gmail.com>
2025-12-31 17:50:22 +0530

    adding sidebar of the deshboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import CreateEmployeeForm from "@/components/createEmployee"
-
+import SideBar from "@/components/SideBar"
 export default function Dashboard(){
     return(
         <div>
             <CreateEmployeeForm/>
+            <SideBar/>
         </div>
     )
 }
