e3e7b40e84bd92ecf5cb2319a485a0620774ae9e
tufail2005 <tufail1526@gmail.com>
2025-12-31 11:17:35 +0530

    31 1st push

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,89 +3,65 @@ import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-export default async function POST(req: Request) {
-    const session = await getServerSession(authOption);
+export async function POST(req: Request) {
+  const session = await getServerSession(authOption);
 
-    if (!session || !session.user) {
-        return NextResponse.json({ error: "you are not log-IN" }, { status: 401 });
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "you are not log-IN" }, { status: 401 });
+  }
+
+  try {
+    const body = await req.json();
+    const {
+      name,
+      phoneNo,
+      userType,
+      gender,
+      email,
+      password,
+      DOB,
+      qualification,
+      address,
+      pincode,
+    } = body;
+
+    console.log(body);
+    if (
+      !name ||
+      !phoneNo ||
+      !userType ||
+      !gender ||
+      !email ||
+      !password ||
+      !DOB ||
+      !qualification ||
+      !address ||
+      !pincode
+    ) {
+      return NextResponse.json(
+        { error: "you are missing inputs field" },
+        { status: 400 }
+      );
     }
 
-    try {
-        const body = await req.json();
-        const {
-        name,
-        phoneNo,
-        role,
-        empId,
-        userType,
-        gender,
-        email,
-        password,
-        DOB,
-        qualification,
-        hireDate,
-        address,
-        pincode,
-        leaves,
-        leaveType,
-        holidays,
-        } = body;
-
-        if (
-        !name ||
-        !phoneNo ||
-        !role ||
-        !empId ||
-        !userType ||
-        !gender ||
-        !email ||
-        !password ||
-        !DOB ||
-        !qualification ||
-        !hireDate ||
-        !address ||
-        !pincode ||
-        !leaves ||
-        !leaveType ||
-        !holidays
-        ) {
-        return NextResponse.json(
-            { error: "you are missing inputs field" },
-            { status: 400 }
-        );}
-
-        const userExists = await prisma.user.findUnique({ where: { email } });
-        if (!userExists){
-            return NextResponse.json(
-            { msg: "employee already exist" },
-            { status: 400 }
-            );
-        }
-            
-        const create_user = await prisma.user.create({
-            data: {
-            name,
-            phoneNo,
-            role,
-            empId,
-            userType,
-            gender,
-            email,
-            password,
-            DOB,
-            qualification,
-            hireDate,
-            address,
-            pincode,
-            leaves,
-            leaveType,
-            holidays,
-            },
-        });
-        return NextResponse.json({
-            msg: "Employee got created",
-        });
-        } catch (err) {
-        return NextResponse.json({ msg: "Server error" + err }, { status: 500 });
+    const userExists = await prisma.user.findFirst({ where: { email } });
+    if (userExists) {
+      return NextResponse.json(
+        { msg: "employee already exist" },
+        { status: 400 }
+      );
     }
+
+    const create_user = await prisma.user.create({
+      data: {
+        ...body,
+        DOB: new Date(body.DOB),
+      },
+    });
+    return NextResponse.json({
+      msg: "Employee got created",
+    });
+  } catch (err) {
+    return NextResponse.json({ msg: "Server error" + err }, { status: 500 });
+  }
 }
